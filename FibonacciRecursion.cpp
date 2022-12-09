333a9f478ded089e23c14accd20dff4c84354442
Varun Kumar Reddy Pothula <117117037+Bunny2906@users.noreply.github.com>
2022-12-10 00:05:14 +0530

    Add files via upload

---
+++ FibonacciRecursion.cpp
@@ -0,0 +1,40 @@
+// Fibonacci series using Recursion
+
+#include<iostream>
+using namespace std;
+int fib(int n)
+{
+    if(n<=1)
+    {
+        return n;
+    }
+    else
+    {
+        return fib(n-2)+fib(n-1);
+    }
+}
+int main()
+{   
+    int n;
+    cout<<"enter no of terms : ";
+    cin>>n;
+    for(int i=0;i<=n;i++)
+    {
+        cout<<fib(i)<<endl;
+    }
+}
+/*
+Sample Output:
+enter no of terms : 10
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+*/
